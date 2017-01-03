b8e03f0bf8f471be78997ef3f644f47b2e93c940
Jordan Scholten <schojo13@gmail.com>
2017-01-03 13:47:01 -0800

    Adjust nav dropdown fields

--- src/client/components/Navbar.jsx
+++ src/client/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {ButtonGroup, DropdownButton, MenuItem, Button, Nav, Navbar, NavItem, NavDropdown} from 'react-bootstrap';
+import {Link} from 'react-router';
 
 const buttonGroupInstance = (
   <ButtonGroup>
@@ -15,18 +16,18 @@ const navbar = (
   <Navbar>
   <Navbar.Header>
     <Navbar.Brand>
-      <a href="#">Meet Tracker</a>
+      <Link to="#">Meet Tracker</Link>
     </Navbar.Brand>
   </Navbar.Header>
   <Nav>
     <NavItem eventKey={1} href="#">Enter Results</NavItem>
     <NavItem eventKey={2} href="#">View Results</NavItem>
-    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-      <MenuItem eventKey={3.1}>Action</MenuItem>
-      <MenuItem eventKey={3.2}>Another action</MenuItem>
-      <MenuItem eventKey={3.3}>Something else here</MenuItem>
+    <NavDropdown eventKey={3} title="Pages" id="basic-nav-dropdown">
+      <MenuItem eventKey={3.1}>Coaches</MenuItem>
+      <MenuItem eventKey={3.2}>Athletes</MenuItem>
+      <MenuItem eventKey={3.3}>Teams</MenuItem>
       <MenuItem divider />
-      <MenuItem eventKey={3.3}>Separated link</MenuItem>
+      <MenuItem eventKey={3.3}>About Meet Tracker</MenuItem>
     </NavDropdown>
   </Nav>
 </Navbar>
