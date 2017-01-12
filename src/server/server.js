52517da09fbcb0448ee0c482134d7301e13f646b
Jordan Scholten <schojo13@gmail.com>
2017-01-12 08:26:17 -0800

    Add meet schema

--- src/server/server.js
+++ src/server/server.js
@@ -13,9 +13,9 @@ var connection = mysql.createConnection({
 app.use(express.static(path.join(__dirname, '../../dist')));
 app.use(bodyParser.json());
 // Communicate with db
-connection.query('USE races');
-app.get('/100m', function(req, res){
-  connection.query('SELECT * from 100m', function (err, rows, fields) {
+connection.query('USE meet');
+app.get('/athletes', function(req, res){
+  connection.query('SELECT * from athletes', function (err, rows, fields) {
     if (err) throw err
     res.send(rows)
   });
@@ -26,34 +26,16 @@ app.get('*', function(req, res) {
 })
 
 app.post('/seed', function(req, res) {
-  // 'source "C:/Users/jack_/Desktop/Hack.js/Thesis/src/server/db/seed.sql"'
-  // // let seedFile = path.resolve(__dirname, 'db/seed.sql');
-  // let query = "INSERT into 100m (athlete, time, school, points)
-  // values ('Data Soong', 11.02, 'TFA', 10);
-  //
-  // INSERT into 100m (athlete, time, school, points) values ('William T. Riker', 11.25, 'RotS', 8);
-  //
-  // INSERT into 100m (athlete, time, school, points)
-  // values ('Jeordi LaForge', 11.27, 'ESB', 6);
-  //
-  // INSERT into 100m (athlete, time, school, points)
-  // values ('Deeana Troi', 11.40, 'RotJ', 4);
-  //
-  // INSERT into 100m (athlete, time, school, points)
-  // values ('Jean-Luc Picard', 11.82, 'ANH', 3);";
   connection.query(query);
 })
 
-app.post('/100m', function(req, res) {
-  // connection.query('INSERT into 100m values ')
+app.post('/athlete', function(req, res) {
   console.log(req.body);
   let data = req.body;
-  if(data.delete) {
-    connection.query('truncate table 100m');
-  } else {
-  let query = `INSERT into 100m (athlete, time, school, points) VALUES ('${data.athlete}', ${data.time}, '${data.school}', ${data.points});`;
-  connection.query(query);
-  }
+  let query = `INSERT into Athletes (athlete_name, team, athlete_points) VALUES (?,?,?);`;
+
+  connection.query(query, ['jack', 2, 400]);
+
   console.log(data)
   res.send('POST for 100m received.');
   // res.json(req.body);
